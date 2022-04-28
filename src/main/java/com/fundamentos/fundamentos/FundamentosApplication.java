ff11b62f05b437d80b478dd56adfe25fd3a4663d
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2022-04-27 21:21:44 -0500

    PropiertiesAndValues

--- src/main/java/com/fundamentos/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/fundamentos/FundamentosApplication.java
@@ -2,6 +2,7 @@ package com.fundamentos.fundamentos;
 
 import com.fundamentos.fundamentos.bean.MyBean;
 import com.fundamentos.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.fundamentos.component.ComponentDependency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -14,13 +15,15 @@ public class FundamentosApplication implements CommandLineRunner {
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
+	private MyBeanWithProperties myBeanWithProperties;
 
 	//Constructor
 	//Inyectamos nuestra dependencia
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency) {
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties) {
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
+		this.myBeanWithProperties = myBeanWithProperties;
 	}
 
 	public static void main(String[] args) {
@@ -32,5 +35,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
+		System.out.println(myBeanWithProperties.function());
 	}
 }
